cbfede207edd0d965b605be21c4186b6739928b1
Kartik Prabhu <katsdaway@gmail.com>
2013-12-04 01:37:41 -0600

    Update webmentionsender.py
    switched to using SoupStrainer to get all 'a' tags. supposed to be more efficient

--- webmentionsender.py
+++ webmentionsender.py
@@ -4,8 +4,8 @@ from PTengine.webmentiontoolsX import WebmentionSend
 
 def send_mentions(article_url, article_contents, default_domain):
     #parse the contents of file now & look for a links and get their hrefs
-    from bs4 import BeautifulSoup
-    link_list = BeautifulSoup(article_contents).find_all('a')
+    from bs4 import BeautifulSoup, SoupStrainer
+    link_list = BeautifulSoup(article_contents, parse_only=SoupStrainer('a'))
     #start sending mentions to each; default site is mine (?)
     for link in link_list:
         WebmentionSend(article_url, urljoin(default_domain, link['href'])).send()
